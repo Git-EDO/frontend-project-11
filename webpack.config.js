ab7b81021a343d6010e22cb51a177031fe9d0476
Dmitry Ezhov <demonex@bk.ru>
2023-10-02 21:55:49 +0400

    prefixer fixed

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
             options: {
               postcssOptions: {
                 plugins: () => [
+                  // eslint-disable-next-line global-require
                   require('autoprefixer'),
                 ],
               },
